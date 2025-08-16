cde1db2f541263f9a17b6b8c4685e6159d29094a
Mohammed Farhan <mohammedfarhan8612@gamil.com>
2025-08-16 19:23:00 +0530

    Add AuthSeller middleware

--- server/routes/seller.route.js
+++ server/routes/seller.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { isSellerAuth, sellerLogin, sellerLogout } from "../controllers/seller.controller.js";
-import authSeller from "../middlewares/AuthSeller.middleware.js";
+import authSeller from "../middlewares/authSeller.middleware.js";
 
 const sellerRouter = express.Router();
 
