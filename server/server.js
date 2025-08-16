7acbc505357640b1cbe54256a6608201861b1cfd
Mohammed Farhan <mohammedfarhan8612@gamil.com>
2025-08-16 19:44:45 +0530

    Add Frontend URL

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectCloudinary();
 
 
 //Allow Multiple Origins
-const allowedOrigins = ["http://localhost:5173", "https://greencart-backend-livid.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "https://greencart-frontend-blond.vercel.app"];
 
 app.post('/stripe', express.raw({ type: 'application/json' }), webhooks)
 
