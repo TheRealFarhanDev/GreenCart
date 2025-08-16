0482b8934292e9384bae60ad6aa6c2330df5b7e5
Mohammed Farhan <mohammedfarhan8612@gamil.com>
2025-08-16 20:57:40 +0530

    Fix auth issue

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import authUser from "../middlewares/authUser.middleware.js";
 import { getAllOrders, getUserOrders, placeOrderCOD, placeOrderStripe } from "../controllers/order.controller.js";
+import authSeller from "../middlewares/authSeller.middleware.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post('/cod', authUser, placeOrderCOD)
 orderRouter.get('/user', authUser, getUserOrders)
-orderRouter.get('/seller', authUser, getAllOrders)
+orderRouter.get('/seller', authSeller, getAllOrders)
 orderRouter.post('/stripe', authUser, placeOrderStripe)
 
 export default orderRouter;
